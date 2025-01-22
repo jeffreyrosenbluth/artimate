abf925a169489d41022e9d9c7e7362df58373598
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.coj>
2025-01-22 14:28:21 -0700

    make cubic go until the end

--- examples/hilbert_swerve.rs
+++ examples/hilbert_swerve.rs
@@ -16,7 +16,7 @@ impl Default for Model {
             order: 6,
             noise: Perlin::default(),
             scale: 0.03,
-            factor: 0.0,
+            factor: 10.0,
             margin: 125.0,
         }
     }
@@ -26,9 +26,9 @@ fn main() -> Result<(), Error> {
     let model = Model::default();
     let n = 2u32.pow(model.order);
     let config = Config::with_dims(1080, 1080)
-        .set_frames(n * n)
-        .set_frames_to_save(n * n);
-    let mut app = App::app(model, config, |_, model| model, draw).set_title("Hilbert Swerve");
+        .set_frames(n * n * 3 / 2)
+        .set_frames_to_save(n * n * 3 / 2);
+    let mut app = App::app(model, config, |_, model| model, draw).set_title("Hilbert");
     app.run()
 }
 
@@ -37,48 +37,93 @@ fn draw(app: &App<AppMode, Model>, model: &Model) -> Vec<u8> {
     canvas.fill(*BLACK);
 
     let n = 2u32.pow(model.order);
+    let n2 = n * n;
+    let m2 = n * n * 3 / 2;
     let mut path = vec![];
 
-    for i in 0..app.frame_count {
-        let j = i as usize;
-        path.push(hilbert(i, model.order));
-        let (w, h) = app.config.wh_f32();
-        let w = w - model.margin * 2.0;
-        let h = h - model.margin * 2.0;
-        let m = w / n as f32;
-        let l = h / n as f32;
-        let s = model.scale;
-        let nx = app.frame_count as f32 / app.config.frames.unwrap() as f32
-            * model.factor
-            * model
-                .noise
-                .get([s * path[j].x as f64, s * path[j].y as f64]) as f32;
-        let ny = app.frame_count as f32 / app.config.frames.unwrap() as f32
-            * model.factor
-            * model
-                .noise
-                .get([123.0 + s * path[j].x as f64, 123.0 + s * path[j].y as f64])
-                as f32;
-        path[j] = pt(m * (path[j].x + nx), l * (path[j].y + ny));
-        path[j] = pt(path[j].x + model.margin, path[j].y + model.margin);
+    if app.frame_count < n2 {
+        for i in 0..app.frame_count.min(n2 - 1) {
+            let j = i as usize;
+            path.push(hilbert(i, model.order));
+            let (w, h) = app.config.wh_f32();
+            let w = w - model.margin * 2.0;
+            let h = h - model.margin * 2.0;
+            let m = w / n as f32;
+            let l = h / n as f32;
+            let s = model.scale;
+            let nx = app.frame_count as f32 / app.config.frames.unwrap() as f32
+                * model.factor
+                * model
+                    .noise
+                    .get([s * path[j].x as f64, s * path[j].y as f64]) as f32;
+            let ny = app.frame_count as f32 / app.config.frames.unwrap() as f32
+                * model.factor
+                * model
+                    .noise
+                    .get([123.0 + s * path[j].x as f64, 123.0 + s * path[j].y as f64])
+                    as f32;
+            path[j] = pt(m * (path[j].x + nx), l * (path[j].y + ny));
+            path[j] = pt(path[j].x + model.margin, path[j].y + model.margin);
+        }
+
+        let p1 = &path[0..app.frame_count as usize / 2].to_vec();
+        Shape::new()
+            .points(&p1)
+            .cubic()
+            .no_fill()
+            .stroke_color(*WHITE)
+            .stroke_weight(2.5)
+            .draw(&mut canvas);
+
+        let p2 = &path[app.frame_count as usize / 2..app.frame_count as usize].to_vec();
+        Shape::new()
+            .points(&p2)
+            .no_fill()
+            .stroke_color(*WHITE)
+            .stroke_weight(2.5)
+            .draw(&mut canvas);
+    } else {
+        for i in 0..n2 {
+            let j = i as usize;
+            path.push(hilbert(i, model.order));
+            let (w, h) = app.config.wh_f32();
+            let w = w - model.margin * 2.0;
+            let h = h - model.margin * 2.0;
+            let m = w / n as f32;
+            let l = h / n as f32;
+            let s = model.scale;
+            let nx = app.frame_count as f32 / app.config.frames.unwrap() as f32
+                * model.factor
+                * model
+                    .noise
+                    .get([s * path[j].x as f64, s * path[j].y as f64]) as f32;
+            let ny = app.frame_count as f32 / app.config.frames.unwrap() as f32
+                * model.factor
+                * model
+                    .noise
+                    .get([123.0 + s * path[j].x as f64, 123.0 + s * path[j].y as f64])
+                    as f32;
+            path[j] = pt(m * (path[j].x + nx), l * (path[j].y + ny));
+            path[j] = pt(path[j].x + model.margin, path[j].y + model.margin);
+        }
+
+        let p3 = &path[0..(app.frame_count - n2 / 2) as usize].to_vec();
+        Shape::new()
+            .points(&p3)
+            .cubic()
+            .no_fill()
+            .stroke_color(*WHITE)
+            .stroke_weight(2.5)
+            .draw(&mut canvas);
+
+        let p4 = &path[(app.frame_count - n2 / 2) as usize..n2 as usize].to_vec();
+        Shape::new()
+            .points(&p4)
+            .no_fill()
+            .stroke_color(*WHITE)
+            .stroke_weight(2.5)
+            .draw(&mut canvas);
     }
-
-    let p1 = &path[0..app.frame_count as usize / 2].to_vec();
-    Shape::new()
-        .points(&p1)
-        .cubic()
-        .no_fill()
-        .stroke_color(*WHITE)
-        .stroke_weight(3.0)
-        .draw(&mut canvas);
-
-    let p2 = &path[app.frame_count as usize / 2..app.frame_count as usize].to_vec();
-    Shape::new()
-        .points(&p2)
-        .no_fill()
-        .stroke_color(*WHITE)
-        .stroke_weight(3.0)
-        .draw(&mut canvas);
 
     canvas.take()
 }
